aaf47ce01a2768920e51a3a34e4a452a1b7e0a8e
asnganje <abd.nganje@gmail.com>
2026-08-19 04:54:06 +0300

    add lambdas basics

--- lambdas.py
+++ lambdas.py
@@ -11,4 +11,21 @@ doubles = list(map(lambda n:n*2, nums))
 # print(doubles)
 people= ["Abdul", "Ozil", "Auf"]
 peeps = list(map(lambda name: name.upper(), people))
-print(peeps)
+# print(peeps)
+aas = list(filter(lambda n:n[0].lower() == "a", people))
+# print(aas)
+
+users = [
+  {"username":"jeff", "tweets":["I love", "I hate"],
+  "username":"joel", "tweets":["Yooh"],
+  "username":"bob", "tweets":[]
+  }
+]
+
+inactive_users = list(filter(lambda user: user["tweets"], users))
+# print(inactive_users)
+instructors = ["abdul", "auf", "mikes"]
+m=list(map(lambda name: f"Your instructor is {name}",
+    filter(lambda value: len(value) < 5, instructors )))
+# print(m)
+
